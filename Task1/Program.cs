e4c39237ae7eb2d0e3b03075fa4ea9d79e62d686
Aleks <korparativ@mail.ru>
2024-01-27 19:38:33 +1200

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+void ShowArrayInterval (int M, int N)
+{
+    if (M == N)
+    {
+        Console.Write (N);
+        return;
+    }
+    Console.Write ($"{M} ");
+    ShowArrayInterval (M+1,N);
+}
+
+ShowArrayInterval (4,8);
